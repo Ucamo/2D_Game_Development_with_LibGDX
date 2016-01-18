34b579bbb0fcb258a8b96cfe0bfc9ddf525c4d10
Uriel Carrillo <carrillouriel@gmail.com>
2016-01-18 01:16:58 -0700

    resolve the cantor gasket excercise

--- 1.3.08-Challenge-DrawTheCantorGasket/core/src/com/udacity/gamedev/drawthecantorgasket/DrawTheCantorGasket.java
+++ 1.3.08-Challenge-DrawTheCantorGasket/core/src/com/udacity/gamedev/drawthecantorgasket/DrawTheCantorGasket.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
+import java.awt.Color;
+
 /*
 
 TODO: Start here
@@ -18,6 +20,7 @@ public class DrawTheCantorGasket extends ApplicationAdapter {
 
     ShapeRenderer shapeRenderer;
     // TODO: Set a constant for how many recursions to draw. 5 is a good place to start
+    final int recursions=5;
 
     @Override
     public void create () {
@@ -34,10 +37,15 @@ public class DrawTheCantorGasket extends ApplicationAdapter {
         Rectangle bounds = findLargestSquare();
 
         // TODO: Begin a filled shapeRenderer batch
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 
         // TODO: Draw a white square matching the bounds
+        shapeRenderer.setColor(com.badlogic.gdx.graphics.Color.WHITE);
+        shapeRenderer.rect(bounds.x,bounds.y,bounds.height,bounds.width);
 
         // TODO: Set the working color to black, and call punchCantorGasket with the bounds
+        shapeRenderer.setColor(com.badlogic.gdx.graphics.Color.BLACK);
+        punchCantorGasket(bounds.x,bounds.y,bounds.height,recursions);
 
         // TODO: End the batch
         shapeRenderer.end();
@@ -46,10 +54,25 @@ public class DrawTheCantorGasket extends ApplicationAdapter {
 
     private void punchCantorGasket(float x, float y, float size, int recursions){
         // TODO: Base case, if recursions = 0, return
+        if(recursions==0)
+        {
+            return;
+        }
 
         // TODO: Draw a black square in the middle square
+        shapeRenderer.rect(x+size/3,y+size/3,size/3,size/3);
 
         // TODO: Call punchCantorGasket on all 8 other squares
+        float newSize = size/3;
+        punchCantorGasket(x, y, newSize, recursions - 1);
+        punchCantorGasket(x + newSize, y, newSize, recursions - 1);
+        punchCantorGasket(x + 2 * newSize, y, newSize, recursions-1);
+        punchCantorGasket(x, y + newSize, newSize, recursions-1);
+        punchCantorGasket(x + 2 * newSize, y + newSize, newSize, recursions - 1);
+        punchCantorGasket(x, y + 2 * newSize, newSize, recursions - 1);
+        punchCantorGasket(x + newSize, y + 2 * newSize, newSize, recursions-1);
+        punchCantorGasket(x + 2 * newSize, y + 2 * newSize, newSize, recursions -1);
+
 
     }
 
