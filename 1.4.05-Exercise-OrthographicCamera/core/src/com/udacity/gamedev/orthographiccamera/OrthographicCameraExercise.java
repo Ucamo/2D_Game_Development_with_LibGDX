6aa198dcbd7257c76d56383a6f5d43fa93331765
Uriel Carrillo <carrillouriel@gmail.com>
2016-01-18 23:28:15 -0700

    Orthographic Camera excercise done, set the view to a ball centered movement

--- 1.4.05-Exercise-OrthographicCamera/core/src/com/udacity/gamedev/orthographiccamera/OrthographicCameraExercise.java
+++ 1.4.05-Exercise-OrthographicCamera/core/src/com/udacity/gamedev/orthographiccamera/OrthographicCameraExercise.java
@@ -3,6 +3,7 @@ package com.udacity.gamedev.orthographiccamera;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
@@ -27,6 +28,7 @@ public class OrthographicCameraExercise extends ApplicationAdapter {
     long timeCreated;
 
     //TODO: Declare an OrthographicCamera
+    OrthographicCamera camera;
 
 
     @Override
@@ -35,9 +37,11 @@ public class OrthographicCameraExercise extends ApplicationAdapter {
         timeCreated = TimeUtils.millis();
 
         // TODO: Initialize the camera
+        camera = new OrthographicCamera();
 
 
         // TODO: Set the camera's position to the center of the circle's movement (X_CENTER, Y_CENTER)
+        camera.position.set(X_CENTER,Y_CENTER,0);
 
     }
 
@@ -50,13 +54,15 @@ public class OrthographicCameraExercise extends ApplicationAdapter {
     public void resize(int width, int height) {
 
         // TODO: Calculate the aspect ratio (width / height)
+        float aspectRatio=1.0f*width/height;
 
 
         // TODO: Set the camera's viewport height taking into account the ball's movement and radius
-
+        camera.viewportHeight = 2 * (Y_AMPLITUDE + BALL_RADIUS);
 
         // TODO: Set the camera's viewport width to maintain the aspect ratio
 
+        camera.viewportWidth = aspectRatio * camera.viewportHeight;
     }
 
     @Override
@@ -65,9 +71,11 @@ public class OrthographicCameraExercise extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // TODO: Call update() on the camera
+        camera.update();
 
 
         // TODO: Set the SceneRenderer's projection matrix equal to the camera's combined matrix
+        renderer.setProjectionMatrix(camera.combined);
 
 
         renderer.begin(ShapeType.Filled);
