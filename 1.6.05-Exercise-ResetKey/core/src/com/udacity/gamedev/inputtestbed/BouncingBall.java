98495144dd2fec0796afea6e0748ad2ea301c68d
Uriel Carrillo <carrillouriel@gmail.com>
2016-01-20 18:52:34 -0700

    Reset Button

--- 1.6.05-Exercise-ResetKey/core/src/com/udacity/gamedev/inputtestbed/BouncingBall.java
+++ 1.6.05-Exercise-ResetKey/core/src/com/udacity/gamedev/inputtestbed/BouncingBall.java
@@ -127,6 +127,10 @@ public class BouncingBall extends InputAdapter {
         }
 
         // TODO: If Keys.R was pressed, call init() to reset the ball
+        if(keycode == Keys.R)
+        {
+            init();
+        }
 
 
         return true;
