bc620dd5ff3038b290f86ad9b081f0a3f22ba7a8
Uriel Carrillo <carrillouriel@gmail.com>
2016-01-21 22:27:38 -0700

    started working on The first icice

--- 1.7.03-Exercise-TheFirstIcicle/core/src/com/udacity/gamedev/icicles/Icicle.java
+++ 1.7.03-Exercise-TheFirstIcicle/core/src/com/udacity/gamedev/icicles/Icicle.java
@@ -1,25 +1,44 @@
 package com.udacity.gamedev.icicles;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+
 public class Icicle {
 
     public static final String TAG = Icicle.class.getName();
 
     // TODO: Add a Vector2 position
+    Vector2 position;
 
 
     // TODO: Add a constructor that sets the position
+    public Icicle()
+    {
+        position= new Vector2(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
+    }
 
     // TODO: Add a render function that takes a ShapeRenderer
-
-
+    public void render(ShapeRenderer shapeRenderer)
+    {
 
         // TODO: Set the ShapeRenderer's color
-
+        shapeRenderer.setColor(Color.CYAN);
 
         // TODO: Set the ShapeType
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 
 
         // TODO: Draw the icicle using the size constants
+        shapeRenderer.circle(10,10,10);
+        shapeRenderer.end();
+
+    }
+
+
+
+
 
 
 }
---
+++ Icicles/core/src/com/udacity/gamedev/icicles/Player.java
@@ -0,0 +1,7 @@
+package com.udacity.gamedev.icicles;
+
+/**
+ * Created by uriel on 21/01/16.
+ */
+public class Player {
+}
