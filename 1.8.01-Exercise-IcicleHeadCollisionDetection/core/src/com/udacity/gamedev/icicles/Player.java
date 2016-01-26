2db1f304d7bd41c72e3de83bb94147a8d6ffdeaa
Uriel Carrillo <carrillouriel@gmail.com>
2016-01-25 20:38:21 -0700

    Icicle Head Collision Detection

--- 1.8.01-Exercise-IcicleHeadCollisionDetection/core/src/com/udacity/gamedev/icicles/Player.java
+++ 1.8.01-Exercise-IcicleHeadCollisionDetection/core/src/com/udacity/gamedev/icicles/Player.java
@@ -52,6 +52,12 @@ public class Player {
         boolean isHit = false;
 
         // TODO: Loop over icicles, checking if the point of any icicle is within the player's head
+        for (Icicle icicle: icicles.icicleList
+             ) {
+            if (icicle.position.dst(position) < Constants.PLAYER_HEAD_RADIUS) {
+                isHit = true;
+            }
+        }
 
 
         return isHit;
